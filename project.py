fc384aa1e0758cf39b5bb0d9cb554e36bc962db1
Chmiel_Michał <92634372+chmielmichal@users.noreply.github.com>
2023-12-20 22:18:26 +0100

    time display + qr code

--- project.py
+++ project.py
@@ -5,9 +5,10 @@ import os
 import pygame
 from pygame.locals import *
 import requests
+from datetime import datetime
+import qrcode
 
-#command to run it for first time   $env:GOOGLE_APPLICATION_CREDENTIALS = "D:\thesisProject\thesisproject-8470d-a832794d2e11.json"
-
+# command to run it for the first time $env:GOOGLE_APPLICATION_CREDENTIALS = "D:\thesisProject\thesisproject-8470d-a832794d2e11.json"
 
 # Initialize Google Cloud Storage client
 client = storage.Client()
@@ -35,45 +36,76 @@ def display_image_from_gcs(object_name):
     # Clean up: Delete the local downloaded image file
     os.remove(local_path)
 
-    # Wait for a short duration before displaying the next image
+    # Display time
     pygame.time.wait(3000)
+
+#QR code
+def generate_qr_code(link):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(link)
+    qr.make(fit=True)
+
+    qr_img = qr.make_image(fill_color="black", back_color="white")
+    return qr_img
 
 # Pygame initialization
 pygame.init()
 screen = pygame.display.set_mode((2560, 1440))
-pygame.display.set_caption("Image Viewer")
+pygame.display.set_caption("INFOKIOSK")
 
-# Function to display an image with frame and weather information
+# Display
 def display_image(file_path):
     image = Image.open(file_path)
 
-    # Adjust frame size and position
-    border_width = 200
-    border_height = 300
-    composite_image = Image.new('RGB', (image.width + border_width, image.height + border_height), 'white')
+    # Frame size and position
+    border_width = int((screen.get_width() - image.width) / 2)
+    border_height = int((screen.get_height() - image.height) / 2)
+    composite_image = Image.new('RGB', (screen.get_width(), screen.get_height()), 'white')
+
+    # Slide position
     composite_image.paste(image, (0, 0))
 
-    # Add frame in the shape of the letter "L"
+    # Borders
     draw = ImageDraw.Draw(composite_image)
-    draw.rectangle([(image.width, 0), (image.width + border_width, image.height)], outline="white", width=border_width)
-    draw.rectangle([(0, image.height), (image.width, image.height + border_height)], outline="white", width=border_width)
+    border_color = (23, 55, 110)
+    draw.rectangle([(image.width, 0), (screen.get_width(), screen.get_height())], fill=border_color)
+    draw.rectangle([(0, image.height), (screen.get_width(), screen.get_height())], fill=border_color)
 
-    # Get weather information from OpenWeatherMap API
+    # Weather information from OpenWeatherMap API
     weather_data = get_weather_data()
     font = ImageFont.load_default()
-    
-    # Adjust font size
+
+    # Font size
     font_size = 20
     font = ImageFont.truetype("arial.ttf", font_size)
 
-    # Add weather information
-    draw.text((50, image.height + border_width + 10), weather_data, font=font, fill="black")
+    # Weather information
+    draw.text((50, image.height + border_height + 10), weather_data, font=font, fill="white")
 
-    # Save temporary file with the result
+    # Time display
+    current_time = datetime.now().strftime("%H:%M")
+    time_font_size = 50
+    time_font = ImageFont.truetype("arial.ttf", time_font_size)
+    text_bbox = draw.textbbox((0, 0), current_time, font=time_font)
+    text_width, text_height = text_bbox[2] - text_bbox[0], text_bbox[3] - text_bbox[1]
+    draw.text((screen.get_width() - text_width - 220, border_height + 10), current_time, font=time_font, fill="white")
+
+    qr_code_link = "https://plany.ath.bielsko.pl"
+    qr_code = generate_qr_code(qr_code_link)
+
+    # Paste the QR code onto the composite image
+    composite_image.paste(qr_code, (screen.get_width() - qr_code.width - 460, screen.get_height() - qr_code.height - 10))
+
+    # Temp image
     result_path = "result.jpg"
     composite_image.save(result_path)
 
-    # Display the image in the Pygame window
+    # Pygame logics
     img = pygame.image.load(result_path)
     screen.blit(img, (0, 0))
     pygame.display.flip()
@@ -81,7 +113,7 @@ def display_image(file_path):
 # OpenWeatherMap API
 def get_weather_data():
     api_key = '84cea28a50728eb44f7ccde38994cb77'
-    city_name = 'Bielsko-Biała'  
+    city_name = 'Chrzanów'  
 
     api_url = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}'
     response = requests.get(api_url)
@@ -89,7 +121,7 @@ def get_weather_data():
 
     # Information
     if 'main' in data and 'temp' in data['main']:
-        temperature = data['main']['temp']
+        temperature = round(data['main']['temp'] - 273.15)
         weather_description = data['weather'][0]['description']
         return f'Temperature: {temperature}°C\nWeather: {weather_description}'
     else:
@@ -104,7 +136,7 @@ try:
             if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
                 running = False
 
-        # Get a list of all objects in the GCS bucket
+        # Get a list of all objects in the Google Cloud Service bucket
         bucket = client.get_bucket(bucket_name)
         blobs = bucket.list_blobs()
 
@@ -118,7 +150,3 @@ except Exception as e:
 
 finally:
     pygame.quit()
-
-
-
-
