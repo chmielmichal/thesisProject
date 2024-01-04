776977c7e9aaec1a5adc5bb49bac3f94dae8f212
Chmiel_Michał <92634372+chmielmichal@users.noreply.github.com>
2024-01-04 15:58:30 +0100

    firebase text display + aqi display

--- project.py
+++ project.py
@@ -53,6 +53,17 @@ def generate_qr_code(link):
     qr_img = qr.make_image(fill_color="black", back_color="white")
     return qr_img
 
+def download_text_file_from_gcs(object_name):
+    # Local path to save the downloaded text file
+    local_path = os.path.join(local_folder, object_name)
+
+    # Download text file from GCS
+    bucket = client.get_bucket(bucket_name)
+    blob = bucket.blob(object_name)
+    blob.download_to_filename(local_path)
+
+    return local_path
+
 # Pygame initialization
 pygame.init()
 screen = pygame.display.set_mode((2560, 1440))
@@ -76,13 +87,36 @@ def display_image(file_path):
     draw.rectangle([(image.width, 0), (screen.get_width(), screen.get_height())], fill=border_color)
     draw.rectangle([(0, image.height), (screen.get_width(), screen.get_height())], fill=border_color)
 
+    text_file_name = "informationtext.txt"
+    text_file_path = download_text_file_from_gcs(text_file_name)
+
+    if os.path.exists(text_file_path):
+        with open(text_file_path, "r", encoding="utf-8") as text_file:
+            text_content = text_file.read()
+            text_font_size = 33
+            text_font = ImageFont.truetype("arial.ttf", text_font_size)
+            text_bbox = draw.textbbox((0, 0), text_content, font=text_font)
+            text_width, text_height = text_bbox[2] - text_bbox[0], text_bbox[3] - text_bbox[1]
+            padding_left = -240
+            draw.text((border_width + padding_left, screen.get_height() - text_height - 150), text_content, font=text_font, fill="white")
+
+
     # Weather information from OpenWeatherMap API
     weather_text = get_weather_data()
-    font_size = 20
+    font_size = 26
     font = ImageFont.truetype("arial.ttf", font_size)
     weather_text_bbox = draw.textbbox((0, 0), weather_text, font=font)
     weather_text_width, weather_text_height = weather_text_bbox[2] - weather_text_bbox[0], weather_text_bbox[3] - weather_text_bbox[1]
-    draw.text((screen.get_width() - weather_text_width - 190, border_height + 340), weather_text, font=font, fill="white")
+    draw.text((screen.get_width() - weather_text_width - 120, border_height + 340), weather_text, font=font, fill="white")
+
+    #Air quality display from iqair
+    air_quality_data = get_air_quality_data()
+    air_quality_font_size = 23
+    air_quality_font = ImageFont.truetype("arial.ttf", air_quality_font_size)
+    air_quality_text_bbox = draw.textbbox((0, 0), air_quality_data, font=air_quality_font)
+    air_quality_text_width, air_quality_text_height = air_quality_text_bbox[2] - air_quality_text_bbox[0], air_quality_text_bbox[3] - air_quality_text_bbox[1]
+    draw.text((screen.get_width() - air_quality_text_width - 200, border_height + 430), air_quality_data, font=air_quality_font, fill="white")
+
 
     # Time display
     current_time = datetime.now().strftime("%H:%M")
@@ -110,7 +144,7 @@ def display_image(file_path):
     qr_code = generate_qr_code(qr_code_link)
 
     # Paste the QR code onto the composite image
-    composite_image.paste(qr_code, (screen.get_width() - qr_code.width - 460, screen.get_height() - qr_code.height - 10))
+    composite_image.paste(qr_code, (screen.get_width() - qr_code.width - 460, screen.get_height() - qr_code.height - 360))
 
     # Temp image
     result_path = "result.jpg"
@@ -123,20 +157,37 @@ def display_image(file_path):
 
 # OpenWeatherMap API
 def get_weather_data():
-    api_key = '84cea28a50728eb44f7ccde38994cb77'
-    city_name = 'Chrzanów'  
+    api_keyweather = '84cea28a50728eb44f7ccde38994cb77'
+    city_name = 'Bielsko-Biała'  
 
-    api_url = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}'
+    api_url = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_keyweather}'
     response = requests.get(api_url)
     data = response.json()
 
-    # Information
+    # Temperature
     if 'main' in data and 'temp' in data['main']:
         temperature = round(data['main']['temp'] - 273.15)
         weather_description = data['weather'][0]['description']
         return f'Temperature: {temperature}°C\nWeather: {weather_description}'
     else:
-        return 'Weather information not available'
+        return 'Prognoza pogody niedostępna'
+    
+#IQWeather api aqi information
+def get_air_quality_data():   
+    api_keyaqi = 'c89a2c3b-7d88-4340-a4a3-1322c7d2d83a'
+    api_url = f'http://api.airvisual.com/v2/city?city=Bielsko-Biala&state=Silesia&country=Poland&key={api_keyaqi}'
+    response = requests.get(api_url)
+    data = response.json()
+    print(data)
+
+    if 'status' in data and data['status'] == 'success':
+        current_data = data.get('data', {}).get('current', {})
+        
+        if 'pollution' in current_data:
+            air_quality_index = current_data['pollution'].get('aqius')
+            return f'Air Quality Index: {air_quality_index}'
+
+    return 'Indeks jakości powietrza niedostępny'
 
 # Main loop
 running = True
